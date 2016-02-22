4f662a9fb0ff34f6ea889e4b8442bb90b1225092
JackWhite20 <phil.westf@web.de>
2016-02-22 21:10:29 +0100

    Added basic client API and impl. Changed from object waiting to a 'CountDownLatch'.

---
+++ Client/src/main/java/de/jackwhite20/cascade/client/Client.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2015 "JackWhite20"
+ *
+ * This file is part of Cascade.
+ *
+ * Cascade is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.jackwhite20.cascade.client;
+
+import de.jackwhite20.cascade.shared.protocol.packet.Packet;
+
+/**
+ * Created by JackWhite20 on 19.02.2016.
+ */
+public interface Client {
+
+    /**
+     * Connects the client with the passed client config.
+     */
+    void connect();
+
+    /**
+     * Disconnects the client and frees all used resources.
+     */
+    void disconnect();
+
+    /**
+     * Returns if the client is connected and running.
+     *
+     * @return true if running otherwise false.
+     */
+    boolean running();
+
+    /**
+     * Sends a packet over TCP (ProtocolType.TCP).
+     *
+     * @param packet the packet.
+     */
+    void send(Packet packet);
+}
