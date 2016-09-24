0cbcaa6b25f20c019b99e25c424edee4f8b8734a
JackWhite20 <phil.westf@web.de>
2016-09-24 10:58:10 +0200

    Made the thread factory for client and server orderly.

---
+++ Shared/src/main/java/de/jackwhite20/cascade/shared/thread/CascadeThreadFactory.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015 "JackWhite20"
+ *
+ * This file is part of Cascade.
+ *
+ * Cascade is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.jackwhite20.cascade.shared.thread;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by JackWhite20 on 24.09.2016.
+ */
+public class CascadeThreadFactory implements ThreadFactory {
+
+    private final AtomicInteger id = new AtomicInteger(0);
+
+    private String baseName;
+
+    public CascadeThreadFactory(String baseName) {
+
+        this.baseName = baseName;
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+
+        Thread thread = new Thread(r);
+        thread.setName("Cascade " + baseName + " Thread #" + id.getAndIncrement());
+
+        return thread;
+    }
+}
